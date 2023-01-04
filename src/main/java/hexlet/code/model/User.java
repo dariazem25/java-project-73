d90a546058946965955b2e20e4d1b48fc203a5e3
Daria Zemlyanaya <zemlyanayadasha78@gmail.com>
2023-01-04 21:46:23 +0300

    fix checkstyle

--- src/main/java/hexlet/code/model/User.java
+++ src/main/java/hexlet/code/model/User.java
@@ -5,7 +5,12 @@ import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
 import java.util.Date;
 
 import static javax.persistence.TemporalType.TIMESTAMP;
